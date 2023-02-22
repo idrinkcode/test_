028d664ad2c5454da9b549d33aca36866dd23166
idrinkcode <sceks96@gmail.com>
2023-02-22 19:37:51 +0900

    연습중

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.max_speed = 100;
+        car1.name = "hyundai";
+
+        Car car2 = new Car();
+        car2.max_speed = 150;
+        car2.name = "audi";
+
+        car1.run();
+        car2.run();
+    }
+}
+class Car{
+    int max_speed;
+    String name;
+
+    void run(){
+        System.out.println(name + "자동차가" + max_speed + "의 속도로 달립니다.");
+    }
+}
